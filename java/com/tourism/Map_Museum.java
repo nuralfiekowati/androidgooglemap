da48b026db0cb42bb82d7a24c13362b8791a0772
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-03-07 12:46:29 +0700

    [added] files
    Upload main files to build android google map

---
+++ java/com/tourism/Map_Museum.java
@@ -0,0 +1,31 @@
+﻿package com.tourism;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by alfi on 10/19/2017.
+ */
+
+public class Map_Museum extends FragmentActivity {
+    private GoogleMap map;
+    private MapFragment mapFragment;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_map);
+     
+        GoogleMap map;
+        map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(52.519467, 13.398745))
+                .title("First Museum"));
+    }
+}
